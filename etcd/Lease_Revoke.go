885804428f4a4befd9777d9f0a90825922926eef
dushaoshuai <1528606126@qq.com>
2021-06-20 10:17:04 +0800

    lease revoke

---
+++ etcd/Lease_Revoke.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+func main() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.Close()
+
+	grantResp, err := cli.Grant(context.TODO(), 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key := "foo"
+	_, err = cli.Put(context.TODO(), key, "bar", clientv3.WithLease(grantResp.ID))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = cli.Revoke(context.TODO(), grantResp.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	getResp, err := cli.Get(context.TODO(), key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("number of keys:", len(getResp.Kvs))
+}
