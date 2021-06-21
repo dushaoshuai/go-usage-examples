4fe480ba52fe33cbcf6d2acbf76c66519e25e6dc
dushaoshuai <1528606126@qq.com>
2021-06-21 14:07:03 +0800

    etcd Mutet_TryLock

---
+++ etcd/Mutex_TryLock.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/concurrency"
+)
+
+func main() {
+	// client
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.Close()
+
+	// session
+	s, err := concurrency.NewSession(cli)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s.Close()
+
+	// mutex
+	m := concurrency.NewMutex(s, "/cron/scanFiles/reload.conf")
+	// acquire lock for s
+	if err := m.TryLock(context.TODO()); err != nil {
+		log.Print(err)
+		return
+	}
+	fmt.Println("acquired lock for session")
+	// do something
+	timer := time.After(10 * time.Second)
+	ticker := time.Tick(1 * time.Second)
+outerLoop:
+	for {
+		fmt.Println("Do something!")
+		<-ticker
+		select {
+		case <-timer:
+			break outerLoop
+		default:
+		}
+	}
+}
---
+++ etcd/distributed_lock.go
@@ -0,0 +1,5 @@
+package main
+
+// for standard implementations of a distributed lock,
+// https://github.com/etcd-io/etcd/blob/release-3.5/client/v3/concurrency/mutex.go
+// https://github.com/etcd-io/etcd/blob/release-3.5/client/v3/concurrency/session.go
