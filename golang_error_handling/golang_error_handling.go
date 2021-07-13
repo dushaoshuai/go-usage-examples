14b25f39ea0c2937fe94281170e642a0c4f01543
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2021-07-13 15:00:09 +0800

    golang-error-handling

---
+++ golang_error_handling/golang_error_handling.go
@@ -0,0 +1,27 @@
+package golang_error_handling
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+)
+
+var skip = 100
+
+func LogError(e error) {
+	if e == nil {
+		return
+	}
+	log.Print(e)
+	// https://github.com/gohouse/e/blob/3cf103d33981f87b78674612d219538c5ea4f624/context.go#L45
+	for i := 1; i < skip; i++ {
+		if function, file, line, ok := runtime.Caller(i); ok {
+			fmt.Println(runtime.FuncForPC(function).Name())
+			fmt.Printf("    %s:%d\n", file, line)
+		} else {
+			break
+		}
+	}
+	os.Exit(1)
+}
