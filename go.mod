f655c8cc396778bcb3ea3bb46e8381afb3f9c3f0
dushaoshuai <1528606126@qq.com>
2021-07-15 15:57:49 +0800

    find resources located relative to an executable using os.Executable()

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module api-examples
 go 1.16
 
 require (
+	github.com/gin-gonic/gin v1.7.2
 	github.com/pkg/errors v0.9.1
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/pflag v1.0.5
--- golang/golang_os/main.go
+++ golang/golang_os/main.go
@@ -4,17 +4,38 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	// the current directory is where you execute the program,
 	// not where the program resides.
-	dir, err := os.Getwd()
+	wd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(dir)
+	fmt.Println(wd)
 
 	// os.ExpandEnv()
 	fmt.Println(os.ExpandEnv("${HOME}"))
+	// Output:
+	// /home/user_name
+
+	// fetch the path name for the executable that started the current process.
+	// The main use case is finding resources located relative to an executable.
+	// go run main.go will get a path started with "/tmp".
+	// Better go build then run.
+	path, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	path, err = filepath.EvalSymlinks(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("the path name for the executable that started the current process is : " + dir)
 }
