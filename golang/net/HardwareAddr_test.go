79a23c72717b4586da47e9ab16b8627f9905aa68
shaouai <1528606126@qq.com>
2022-04-03 10:09:40 +0800

    trivial

---
+++ golang/net/HardwareAddr_test.go
@@ -0,0 +1,16 @@
+package net_test
+
+import (
+	"fmt"
+	"net"
+)
+
+func Example_HardwareAddr() {
+	macAddr, err := net.ParseMAC("98:01:a7:b3:c6:5f")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(macAddr)
+	// Output:
+	// 98:01:a7:b3:c6:5f
+}
--- golang/unsafe/unsafe_test.go
+++ golang/unsafe/unsafe_test.go
@@ -1,9 +1,5 @@
 package unsafe_test
 
-import "fmt"
-
 func Example_unsafe() {
-	a := interface{}(5)
-	fmt.Printf("%v %[1]T\n", a)
 	// Output:
 }
