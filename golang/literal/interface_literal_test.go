184a02c9b2a6a49e73069400fdecb6084f849011
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2022-05-10 17:04:40 +0800

    go :: interface literal

---
+++ golang/literal/interface_literal_test.go
@@ -0,0 +1,23 @@
+package literal_test
+
+import "fmt"
+
+type implementEcho struct{}
+
+func (ie implementEcho) echo() {
+	fmt.Println("echo ech ec e ...")
+}
+
+func Example_interface_literal() {
+	var ie implementEcho
+	var ifce interface{} = ie
+
+	if echo, ok := ifce.(interface {
+		echo()
+	}); ok {
+		echo.echo()
+	}
+
+	// Output:
+	// echo ech ec e ...
+}
