5888e4e2e8e53d999fb23f5f44b882d6a40db0a3
shaouai <1528606126@qq.com>
2022-08-14 21:31:33 +0800

    protobuf :: gen pb script

--- protobuf/gen_pb.sh
+++ protobuf/gen_pb.sh
@@ -6,4 +6,4 @@ DST_DIR=$SRC_DIR
 protoc --proto_path=$SRC_DIR \
   --go_out=$DST_DIR --go_opt=paths=source_relative \
   --go-grpc_out=$DST_DIR --go-grpc_opt=paths=source_relative \
-  $SRC_DIR/any.proto
+  $SRC_DIR/*.proto
