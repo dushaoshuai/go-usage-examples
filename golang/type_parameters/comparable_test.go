4bb116181483979d283672b5e650f1878e4aec35
shaouai <1528606126@qq.com>
2022-09-25 22:11:08 +0800

    golang :: comparable

---
+++ golang/type_parameters/comparable_test.go
@@ -0,0 +1,31 @@
+package type_parameters
+
+// comparable is an interface that is implemented by all comparable types (booleans, numbers, strings, pointers, channels, arrays of comparable types, structs whose fields are all comparable types).
+// The comparable interface may only be used as a type parameter constraint, not as the type of a variable.
+
+import (
+	"fmt"
+	"sort"
+)
+
+func MapKeys[K comparable, V any](m map[K]V) []K {
+	var s []K
+	for k := range m {
+		s = append(s, k)
+	}
+	return s
+}
+
+func ExampleComparable() {
+	keys := MapKeys(map[string]int{
+		"a": 10,
+		"b": 11,
+		"c": 12,
+	})
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
+	fmt.Println(keys)
+	// Output:
+	// [a b c]
+}
