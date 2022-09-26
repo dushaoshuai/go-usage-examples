8ee9e2deb28fb2f1f2644f7536c185a96e60c0d0
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2022-09-26 22:08:09 +0800

    golang :: 接受 context.Context 的函数必须主动发现 context.Context 已经取消

---
+++ golang/context/deadline_exceeded_test.go
@@ -0,0 +1,29 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func Example_ctx() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := acceptCtx(ctx); err != nil {
+		fmt.Println(err)
+	}
+
+	// Output:
+	// context deadline exceeded
+}
+
+func acceptCtx(ctx context.Context) error {
+	select {
+	case <-time.After(13 * time.Second):
+		fmt.Println("13 seconds passed")
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
