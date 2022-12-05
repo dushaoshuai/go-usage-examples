610077425a9c28b7461b731621b34ce921fc46d2
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2022-12-05 11:12:35 +0800

    Phonenumbers (#4)
    * phonenumbers :: get phone number type
    
    * phonenumbers :: get phone number type
    
    * phonesnumbers :: number type, format number and get example number

---
+++ phonenumbers/type_test.go
@@ -0,0 +1,26 @@
+package phonenumbers
+
+import (
+	"fmt"
+
+	"github.com/nyaruka/phonenumbers"
+)
+
+func ExamplePhoneNumberType() {
+	// 填移动号码
+	fmt.Println(getNumberType("xxxxxxxx"))
+	// 填座机号码
+	fmt.Println(getNumberType("02081167888"))
+
+	// Output:
+	// 1
+	// 0
+}
+
+func getNumberType(numberToParse string) phonenumbers.PhoneNumberType {
+	number, err := phonenumbers.Parse(numberToParse, "CN")
+	if err != nil {
+		panic(err)
+	}
+	return phonenumbers.GetNumberType(number)
+}
