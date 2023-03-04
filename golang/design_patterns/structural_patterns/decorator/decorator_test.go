24e5392eb41f44fad93d57eefcdff2960771e5cb
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2023-03-04 15:43:21 +0800

    fix: go package import path

--- golang/design_patterns/structural_patterns/decorator/decorator_test.go
+++ golang/design_patterns/structural_patterns/decorator/decorator_test.go
@@ -1,12 +1,15 @@
 package decorator_test
 
-import "api-examples/golang/design_patterns/structural_patterns/decorator/decorator"
+import "github.com/dushaoshuai/go-usage-examples/golang/design_patterns/structural_patterns/decorator/decorator"
 
 func Example_decorator() {
 	var a decorator.Component
 	a = decorator.NewConcreteComponentA()
 	a = decorator.DecorateByA(a)
 	a = decorator.DecorateByB(a)
+	a = decorator.DecorateByB(a)
+	a = decorator.DecorateByA(a)
+	a = decorator.DecorateByA(a)
 	a.OperationA()
 	a.OperationB()
 
