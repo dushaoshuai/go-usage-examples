90f9fa145248b9540685ab08f6bf6b89c8ce09ad
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2023-03-05 19:27:48 +0800

    Go: 添加注释"接口速度还是比较慢的啊"

--- golang/interface/benchmark_test.go
+++ golang/interface/benchmark_test.go
@@ -49,16 +49,18 @@ func BenchmarkBenchmarkDynamicDispatch_struct(b *testing.B) {
 }
 
 // https://draveness.me/golang/docs/part2-foundation/ch04-basic/golang-interface/#%E5%9F%BA%E5%87%86%E6%B5%8B%E8%AF%95
+// 接口速度还是比较慢的啊
 
 // goos: darwin
 // goarch: amd64
-// pkg: api-examples/golang/interface
+// pkg: github.com/dushaoshuai/go-usage-examples/golang/interface
 // cpu: Intel(R) Core(TM) i7-4770HQ CPU @ 2.20GHz
 // BenchmarkDirectCall_pointer
-// BenchmarkDirectCall_pointer-8                	1000000000	         0.4822 ns/op
+// BenchmarkDirectCall_pointer-8                	1000000000	         0.3399 ns/op
 // BenchmarkDynamicDispatch_pointer
-// BenchmarkDynamicDispatch_pointer-8           	415445734	         2.835 ns/op
+// BenchmarkDynamicDispatch_pointer-8           	576121688	         1.949 ns/op
 // BenchmarkDirectCall_struct
-// BenchmarkDirectCall_struct-8                 	1000000000	         0.4986 ns/op
+// BenchmarkDirectCall_struct-8                 	1000000000	         0.3276 ns/op
 // BenchmarkBenchmarkDynamicDispatch_struct
-// BenchmarkBenchmarkDynamicDispatch_struct-8   	400639581	         3.046 ns/op
+// BenchmarkBenchmarkDynamicDispatch_struct-8   	561326664	         2.104 ns/op
+// PASS
