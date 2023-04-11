123efa5c8e852164cb6f98f4277867ea6bceab84
dushaoshuai <1528606126@qq.com>
2023-04-11 22:23:01 +0800

    *testing.T more methods

--- testing/testing/Test/Test_test.go
+++ testing/testing/Test/Test_test.go
@@ -1,9 +1,39 @@
 package Test_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
+
+func TestLogFail(t *testing.T) {
+	// For tests, the text will be printed only if the test fails or the -test.v flag is set.
+	t.Fail()
+	t.Log("one two three")
+
+	// $ go test -test.run TestLogFail
+	// --- FAIL: TestLogFail (0.00s)
+	//    Test_test.go:10: one two three
+	// FAIL
+	// exit status 1
+	// FAIL    github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.002s
+}
+
+func TestLogVFlag(t *testing.T) {
+	// For tests, the text will be printed only if the test fails or the -test.v flag is set.
+	t.Log("one two three")
+
+	// $ go test -test.v -test.run TestLogVFlag
+	// === RUN   TestLogVFlag
+	//    Test_test.go:23: one two three
+	// --- PASS: TestLogVFlag (0.00s)
+	// PASS
+	// ok      github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.002s
+
+	// $ go test -test.run TestLogVFlag
+	// PASS
+	// ok      github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.002s
+}
 
 func TestCleanup(t *testing.T) {
 	t.Cleanup(func() {
@@ -57,3 +87,133 @@ func TestDeadline(t *testing.T) {
 	// PASS
 	// ok      github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.002s
 }
+
+func TestError(t *testing.T) {
+	t.Log("one")
+	// Error is equivalent to Log followed by Fail.
+	t.Error("two")
+
+	// $ go test -test.run TestError
+	// --- FAIL: TestError (0.00s)
+	//    Test_test.go:91: one
+	//    Test_test.go:93: two
+	// FAIL
+	// exit status 1
+	// FAIL    github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.001s
+}
+
+func TestFailNow(t *testing.T) {
+	// FailNow marks the function as having failed and stops its execution by calling runtime.Goexit.
+	t.FailNow()
+	t.Log("Unreachable code")
+
+	// $ go test -test.run TestFailNow
+	// --- FAIL: TestFailNow (0.00s)
+	// FAIL
+	// exit status 1
+	// FAIL    github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.003s
+}
+
+func TestFailed(t *testing.T) {
+	if t.Failed() {
+		t.Fatalf("t.Failed() = %v, want %v", t.Failed(), false)
+	}
+	t.Error("t.Fail()")
+	if !t.Failed() {
+		t.Fatalf("t.Failed() = %v, want %v", t.Failed(), true)
+	}
+
+	// $ go test -test.run TestFailed
+	// --- FAIL: TestFailed (0.00s)
+	//    Test_test.go:120: t.Fail()
+	// FAIL
+	// exit status 1
+	// FAIL    github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.001s
+}
+
+func TestFatal(t *testing.T) {
+	t.Fatal("t.Fatal()")
+	t.Log("Unreachable code")
+
+	// $ go test -test.run TestFatal
+	// --- FAIL: TestFatal (0.00s)
+	//    Test_test.go:134: t.Fatal()
+	// FAIL
+	// exit status 1
+	// FAIL    github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.002s
+}
+
+func TestHelper(t *testing.T) {
+	t.Helper() // 暂时没看明白这个函数的作用是什么
+	t.Error("t.Helper() called")
+
+	// $ go test -test.run TestHelper
+	// --- FAIL: TestHelper (0.00s)
+	//    Test_test.go:147: t.Helper() called
+	// FAIL
+	// exit status 1
+	// FAIL    github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.002s
+}
+
+func TestName(t *testing.T) {
+	t.Log(t.Name())
+	for i := 0; i < 2; i++ {
+		t.Run("", func(t *testing.T) {
+			t.Log(t.Name())
+		})
+	}
+	for i := 0; i < 2; i++ {
+		t.Run(fmt.Sprintf("subtest-%v", i), func(t *testing.T) {
+			t.Log(t.Name())
+		})
+	}
+
+	// $ go test -test.v -test.run TestName
+	// === RUN   TestName
+	//    Test_test.go:159: TestName
+	// === RUN   TestName/#00
+	//    Test_test.go:162: TestName/#00
+	// === RUN   TestName/#01
+	//    Test_test.go:162: TestName/#01
+	// === RUN   TestName/subtest-0
+	//    Test_test.go:167: TestName/subtest-0
+	// === RUN   TestName/subtest-1
+	//    Test_test.go:167: TestName/subtest-1
+	// --- PASS: TestName (0.00s)
+	//    --- PASS: TestName/#00 (0.00s)
+	//    --- PASS: TestName/#01 (0.00s)
+	//    --- PASS: TestName/subtest-0 (0.00s)
+	//    --- PASS: TestName/subtest-1 (0.00s)
+	// PASS
+	// ok      github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.002s
+}
+
+func TestRun(t *testing.T) {
+	for i := 0; i < 6; i++ {
+		if !t.Run("", func(t *testing.T) {
+			if i%2 == 0 {
+				t.SkipNow() // SkipNow will not mark the test as having failed as FailNow.
+			}
+			t.Parallel() // Run reports whether f succeeded (or at least did not fail before calling t.Parallel).
+			t.Error("failed in parallel test after calling t.Parallel")
+		}) {
+			t.Fatal("t.Run() = false, want true")
+		}
+	}
+
+	// $ go test -test.run TestRun
+	// --- FAIL: TestRun (0.00s)
+	//    --- FAIL: TestRun/#01 (0.00s)
+	//        Test_test.go:198: failed in parallel test after calling t.Parallel
+	//    --- FAIL: TestRun/#05 (0.00s)
+	//        Test_test.go:198: failed in parallel test after calling t.Parallel
+	//    --- FAIL: TestRun/#03 (0.00s)
+	//        Test_test.go:198: failed in parallel test after calling t.Parallel
+	// FAIL
+	// exit status 1
+	// FAIL    github.com/dushaoshuai/go-usage-examples/testing/testing/Test   0.002s
+}
+
+// TODO
+// Setenv
+// TempDir
