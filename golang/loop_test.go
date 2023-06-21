9872b488c839070fab3e212d9fea6fea1bd5ec38
dushaoshuai <1528606126@qq.com>
2023-06-21 22:29:27 +0800

    Go: i in for loop

---
+++ golang/loop_test.go
@@ -0,0 +1,31 @@
+package golang_test
+
+import "fmt"
+
+func Example_loop_i() {
+	for i := 0; i < 10; i++ {
+		i = 9
+		fmt.Println(i)
+	}
+
+	fmt.Println()
+	for i := 0; i < 10; i++ {
+		i := i
+		i = -1
+		fmt.Println(i)
+	}
+
+	// Output:
+	// 9
+	//
+	// -1
+	// -1
+	// -1
+	// -1
+	// -1
+	// -1
+	// -1
+	// -1
+	// -1
+	// -1
+}
