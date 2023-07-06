aa393157d24d7f173d9ee362aa48405be98eb7bf
dushaoshuai <1528606126@qq.com>
2023-07-06 13:49:15 +0800

    test chan

--- golang/channel/must_tested_in_main_func/close_chan_when_there_are_senders/main.go
+++ golang/channel/must_tested_in_main_func/close_chan_when_there_are_senders/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sync"
+	"time"
 
 	"github.com/dushaoshuai/goloop"
 )
@@ -9,31 +10,31 @@ import (
 func main() {
 	ch := make(chan int)
 
-	var wg sync.WaitGroup
+	var wg1 sync.WaitGroup
+	var wg2 sync.WaitGroup
 	for range goloop.Repeat(3) {
-		wg.Add(1)
+		wg1.Add(1)
+		wg2.Add(1)
 		go func() {
-			defer wg.Done()
+			defer wg1.Done()
+			wg2.Done()
 			ch <- 0
 		}()
 	}
 
-	close(ch) // Note: if there are still senders, they will panic after the channel is closed
-	wg.Wait()
+	wg2.Wait()
+	time.Sleep(3 * time.Second) // wait until all three senders are blocked on this channel
+	close(ch)                   // Note: if there are still senders, they will panic after the channel is closed
+	wg1.Wait()
 
 	// Output:
+	// $ go run main.go
 	// panic: send on closed channel
 	//
-	// goroutine 8 [running]:
+	// goroutine 7 [running]:
 	// main.main.func1()
-	//        /home/shaouai/dev/github.com/dushaoshuai/go-usage-examples/golang/channel/must_tested_in_main_func/close_chan_when_there_are_senders/main.go:17 +0x65
+	//        /home/shaouai/dev/github.com/dushaoshuai/go-usage-examples/golang/channel/must_tested_in_main_func/close_chan_when_there_are_senders/main.go:21 +0x7e
 	// created by main.main
-	//        /home/shaouai/dev/github.com/dushaoshuai/go-usage-examples/golang/channel/must_tested_in_main_func/close_chan_when_there_are_senders/main.go:15 +0x52
-	// panic: send on closed channel
-	//
-	// goroutine 6 [running]:
-	// main.main.func1()
-	//        /home/shaouai/dev/github.com/dushaoshuai/go-usage-examples/golang/channel/must_tested_in_main_func/close_chan_when_there_are_senders/main.go:17 +0x65
-	// created by main.main
-	//        /home/shaouai/dev/github.com/dushaoshuai/go-usage-examples/golang/channel/must_tested_in_main_func/close_chan_when_there_are_senders/main.go:15 +0x52
+	//        /home/shaouai/dev/github.com/dushaoshuai/go-usage-examples/golang/channel/must_tested_in_main_func/close_chan_when_there_are_senders/main.go:18 +0x65
+	// exit status 2
 }
