d53d717505154f779d48aabb3baab432c4e9e914
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2023-08-06 15:30:12 +0800

    upgrade protoc, proto-gen-go, proto-gen-go-grpc

--- grpc/helloworld/client/main.go
+++ grpc/helloworld/client/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"api-examples/grpc/helloworld/proto"
 	"context"
 	"flag"
 	"log"
 	"time"
+
+	"github.com/dushaoshuai/go-usage-examples/grpc/helloworld/proto"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
