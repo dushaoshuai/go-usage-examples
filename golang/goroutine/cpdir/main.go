f86f40a0fa6e8b94d90dbe90ac2c6724ad7b144c
dushaoshuai <1528606126@qq.com>
2023-11-11 16:59:36 +0800

    feat(interview question): goroutines
    use 48 goroutines to copy files from the /src/ directory to the /dst/ directory

--- golang/goroutine/cpdir/main.go
+++ golang/goroutine/cpdir/main.go
@@ -6,6 +6,8 @@ import (
 	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
+	"sync"
 )
 
 var (
@@ -22,15 +24,26 @@ func init() {
 }
 
 type file struct {
-	srcDir string
-	dstDir string
-	file   string
+	fullPath    string
+	perm        fs.FileMode
+	dstFullPath string
 }
 
 func main() {
-	fileChan := make(chan file, 20)
+	err := checkDir()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var (
+		fileChan = make(chan file, 20)
+		wg       sync.WaitGroup
+	)
 	for i := 0; i < 48; i++ {
+		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			defer func() {
 				err := recover()
 				if err != nil {
@@ -47,20 +60,39 @@ func main() {
 		}()
 	}
 
-	err := checkDir()
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	_ = filepath.WalkDir(*src,
+		func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				log.Println(err)
+				if d.IsDir() {
+					return fs.SkipDir
+				}
+				return nil
+			}
 
+			err = walkDirFunc(path, d, fileChan)
+			if err != nil {
+				log.Println(err)
+				if d.IsDir() {
+					return fs.SkipDir
+				}
+				return nil
+			}
+
+			return nil
+		},
+	)
+
+	close(fileChan)
+	wg.Wait()
+	log.Println("cpdir: Done")
 }
 
 func checkDir() error {
-	srcEntries, err := os.ReadDir(*src)
+	_, err := os.ReadDir(*src)
 	if err != nil {
 		return err
 	}
-	_ = srcEntries
 
 	_, err = os.ReadDir(*dst)
 	if err != nil && !errors.Is(err, fs.ErrNotExist) {
@@ -71,5 +103,37 @@ func checkDir() error {
 }
 
 func cpFile(f file) error {
+	data, err := os.ReadFile(f.fullPath)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(f.dstFullPath, data, f.perm)
+}
 
+func walkDirFunc(path string, d fs.DirEntry, fileChan chan<- file) error {
+	rel, err := filepath.Rel(*src, path)
+	if err != nil {
+		return nil
+	}
+	dstFullPath := filepath.Join(*dst, rel)
+
+	fileInfo, err := d.Info()
+	if err != nil {
+		return err
+	}
+
+	if d.IsDir() {
+		err = os.MkdirAll(dstFullPath, fileInfo.Mode().Perm())
+		if err != nil {
+			return err
+		}
+	} else {
+		fileChan <- file{
+			fullPath:    path,
+			perm:        fileInfo.Mode().Perm(),
+			dstFullPath: dstFullPath,
+		}
+	}
+
+	return nil
 }
