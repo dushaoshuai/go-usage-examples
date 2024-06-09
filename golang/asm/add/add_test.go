4118230193e5813b6b8ed6eb730b4eac9a9d36ce
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2024-06-09 12:14:11 +0800

    feat(go asm): func Add(a, b int) int

---
+++ golang/asm/add/add_test.go
@@ -0,0 +1,28 @@
+package add
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"<0", args{0, -1}, -1},
+		{"==0", args{-1, 1}, 0},
+		{">0", args{1, 0}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
