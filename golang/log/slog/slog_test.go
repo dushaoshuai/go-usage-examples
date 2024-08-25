a7e3d4bff4670a5af863d34cf61bcc23fc1a679f
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2024-08-25 17:20:46 +0800

    feat(log/slog): simple use of JSONHandler

--- golang/log/slog/slog_test.go
+++ golang/log/slog/slog_test.go
@@ -28,12 +28,17 @@ func Example_json_handler() {
 	slog.Debug("a debug level msg", "x1", 1, slog.Int("x2", 2))
 	slog.DebugContext(context.Background(), "a debug level msg", "x1", 1, slog.String("x2", "x2 value"))
 
+	slog.Info("a info level msg", "x1", 1, slog.Int("x2", 2))
+	slog.InfoContext(context.Background(), "a info level msg", "x1", 1, slog.Int("x2", 2))
+
 	slog.Warn("a warn level msg", "x1", 1, slog.Int("x2", 2))
 	slog.WarnContext(context.Background(), "a warn level msg", "x1", 1, slog.Int("x2", 2))
 
 	// Output:
-	// {"time":"2024-08-25T16:50:07.000012234+08:00","level":"DEBUG","source":{"function":"github.com/dushaoshuai/go-usage-examples/golang/log/slog_test.Example_json_handler","file":"/tmp/go-usage-examples/golang/log/slog/slog_test.go","line":27},"msg":"a debug level msg","x1":1,"x2":2}
-	// {"time":"2024-08-25T16:50:07.000012234+08:00","level":"DEBUG","source":{"function":"github.com/dushaoshuai/go-usage-examples/golang/log/slog_test.Example_json_handler","file":"/tmp/go-usage-examples/golang/log/slog/slog_test.go","line":28},"msg":"a debug level msg","x1":1,"x2":"x2 value"}
-	// {"time":"2024-08-25T16:50:07.000012234+08:00","level":"WARN","source":{"function":"github.com/dushaoshuai/go-usage-examples/golang/log/slog_test.Example_json_handler","file":"/tmp/go-usage-examples/golang/log/slog/slog_test.go","line":30},"msg":"a warn level msg","x1":1,"x2":2}
-	// {"time":"2024-08-25T16:50:07.000012234+08:00","level":"WARN","source":{"function":"github.com/dushaoshuai/go-usage-examples/golang/log/slog_test.Example_json_handler","file":"/tmp/go-usage-examples/golang/log/slog/slog_test.go","line":31},"msg":"a warn level msg","x1":1,"x2":2}
+	// {"time":"2024-08-25T16:50:07.000012234+08:00","level":"DEBUG","source":{"function":"github.com/dushaoshuai/go-usage-examples/golang/log/slog_test.Example_json_handler","file":"/tmp/go-usage-examples/golang/log/slog/slog_test.go","line":28},"msg":"a debug level msg","x1":1,"x2":2}
+	// {"time":"2024-08-25T16:50:07.000012234+08:00","level":"DEBUG","source":{"function":"github.com/dushaoshuai/go-usage-examples/golang/log/slog_test.Example_json_handler","file":"/tmp/go-usage-examples/golang/log/slog/slog_test.go","line":29},"msg":"a debug level msg","x1":1,"x2":"x2 value"}
+	// {"time":"2024-08-25T16:50:07.000012234+08:00","level":"INFO","source":{"function":"github.com/dushaoshuai/go-usage-examples/golang/log/slog_test.Example_json_handler","file":"/tmp/go-usage-examples/golang/log/slog/slog_test.go","line":31},"msg":"a info level msg","x1":1,"x2":2}
+	// {"time":"2024-08-25T16:50:07.000012234+08:00","level":"INFO","source":{"function":"github.com/dushaoshuai/go-usage-examples/golang/log/slog_test.Example_json_handler","file":"/tmp/go-usage-examples/golang/log/slog/slog_test.go","line":32},"msg":"a info level msg","x1":1,"x2":2}
+	// {"time":"2024-08-25T16:50:07.000012234+08:00","level":"WARN","source":{"function":"github.com/dushaoshuai/go-usage-examples/golang/log/slog_test.Example_json_handler","file":"/tmp/go-usage-examples/golang/log/slog/slog_test.go","line":34},"msg":"a warn level msg","x1":1,"x2":2}
+	// {"time":"2024-08-25T16:50:07.000012234+08:00","level":"WARN","source":{"function":"github.com/dushaoshuai/go-usage-examples/golang/log/slog_test.Example_json_handler","file":"/tmp/go-usage-examples/golang/log/slog/slog_test.go","line":35},"msg":"a warn level msg","x1":1,"x2":2}
 }
