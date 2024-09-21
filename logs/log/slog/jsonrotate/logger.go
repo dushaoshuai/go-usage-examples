a0e18931f5941167895a39e823b2a52151e4e1ce
dushaoshuai <1528606126@qq.com>
2024-09-21 13:56:19 +0800

    feat(log/slog): json rotate logger

---
+++ logs/log/slog/jsonrotate/logger.go
@@ -0,0 +1,30 @@
+package jsonrotate
+
+import (
+	"log/slog"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+var (
+	logLevelVar = new(slog.LevelVar) // Info by default
+)
+
+func setJsonRotateLogger() (closeUnderlyingWriter func()) {
+	logFile := "/tmp/jsonrotatelogger/jsonratote.log"
+
+	dailyRotateRule := logx.DefaultRotateRule(logFile, "-", 10, true)
+	logWriter, err := logx.NewLogger(logFile, dailyRotateRule, true)
+	if err != nil {
+		panic(err)
+	}
+
+	jsonHandler := slog.NewJSONHandler(logWriter, &slog.HandlerOptions{
+		AddSource:   true,
+		Level:       logLevelVar,
+		ReplaceAttr: nil,
+	})
+	slog.SetDefault(slog.New(jsonHandler))
+
+	return func() { logWriter.Close() }
+}
