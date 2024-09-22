529ed1bc96623b0b6742cb844eddb0b1fe27939a
dushaoshuai <1528606126@qq.com>
2024-09-22 19:11:38 +0800

    Add mock package and adjust test for logger rotation
    - Incorporate `mock` package for testing purposes.
    - Update `Test_rotate_logger` to include string logging and address test limitations due to asynchronous rotation logic.

--- logs/log/slog/jsonrotate/logger_test.go
+++ logs/log/slog/jsonrotate/logger_test.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"strconv"
 	"testing"
+	"time"
+
+	"github.com/xhd2015/xgo/runtime/mock"
 )
 
 func TestMain(m *testing.M) {
@@ -31,17 +34,22 @@ func Test_json_logger(t *testing.T) {
 	}
 }
 
-// func Test_rotate_logger(t *testing.T) {
-// 	tomorrow := time.Now().AddDate(0, 0, 1)
-// 	mock.Patch(time.Now, func() time.Time {
-// 		return tomorrow
-// 	})
-//
-// 	ctx := context.Background()
-//
-// 	for i := range 10 {
-// 		slog.LogAttrs(ctx, slog.LevelWarn, "hello tomorrow",
-// 			slog.Int("count", i),
-// 		)
-// 	}
-// }
+func Test_rotate_logger(t *testing.T) {
+	// The actual write operation and rotation happen in a separate goroutine,
+	// so the patch here can't affect them (https://github.com/xhd2015/xgo/tree/master/runtime/mock#:~:text=the%20first%20parameter-,Scope,-Based%20on%20the),
+	// making unit testing quite tricky.
+	// As a result, this unit test is not successful.
+	tomorrow := time.Now().AddDate(0, 0, 1)
+	mock.Patch(time.Now, func() time.Time {
+		return tomorrow
+	})
+
+	ctx := context.Background()
+
+	for i := range 10 {
+		slog.LogAttrs(ctx, slog.LevelWarn, "hello tomorrow",
+			slog.Int("int", i),
+			slog.String("str", strconv.Itoa(i)),
+		)
+	}
+}
