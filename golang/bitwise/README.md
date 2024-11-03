f97d1b18abf1c0e2e3bb421b87edbfb6730860b2
dushaoshuai <1528606126@qq.com>
2024-11-03 22:19:58 +0800

    feat: C bitwise negation

---
+++ golang/bitwise/README.md
@@ -0,0 +1,14 @@
+# Bitwise Operators 按位运算符
+
+## Bitwise Logical Operators 按位逻辑运算符
+
+这些操作符把他们的操作数当作位模式（0 和 1 组成的比特串），没有算术进位和正负的概念。
+
+## Bitwise Negation, One's Complement, Bitwise Complement
+
+0 -> 1, 1 -> 0
+
+C: ~
+Go: ^
+
+## Bitwise Shift Operators 移位远算符
---
+++ golang/bitwise/integer_bits.c
@@ -0,0 +1,39 @@
+// Assumes little endian
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "integer_bits.h"
+
+// https://stackoverflow.com/a/3974138
+char *integerBits(size_t const size, void const *const ptr)
+{
+    unsigned char *b = (unsigned char *)ptr;
+    unsigned char byte;
+    int i, j;
+
+    size_t str_size = size * 8 + 1;
+    char *bit_string = (char *)malloc(str_size);
+    if (bit_string == NULL)
+    {
+        return NULL;
+    }
+
+    memset(bit_string, 0, str_size);
+    size_t index = 0;
+
+    for (i = size - 1; i >= 0; i--)
+    {
+        for (j = 7; j >= 0; j--)
+        {
+            byte = (b[i] >> j) & 1;
+            if (index != 0 || byte != 0)
+            {
+                bit_string[index++] = byte + '0';
+            }
+        }
+    }
+    bit_string[index] = '\0';
+
+    return bit_string;
+}
