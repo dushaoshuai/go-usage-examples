cc4077ae17edd2f40cd3ce484d6fb980b87e63b7
dushaoshuai <1528606126@qq.com>
2024-11-09 18:47:08 +0800

    feat: turning bits on

--- golang/bitwise_op/usage.c
+++ golang/bitwise_op/usage.c
@@ -3,6 +3,7 @@
 #include "integer_bits.h"
 
 void mask();
+void turning_bits_on();
 
 // $ gcc ./usage.c ./integer_bits.c && ./a.out
 int main(int argc, char const *argv[])
@@ -11,6 +12,11 @@ int main(int argc, char const *argv[])
     // 1111 & 11010011 = 11
     // 11111000 & 11010011 = 11010000
     mask();
+
+	// 10101111 | 11010011 = 11111111
+    // 1111 | 11010011 = 11011111
+    // 11111000 | 11010011 = 11111011
+    turning_bits_on();
 
     return 0;
 }
@@ -32,3 +38,22 @@ void mask()
     printf("%s & %s = %s\n", integerBits(sizeof(flags2), &flags1), integerBits(sizeof(mask), &mask), integerBits(sizeof(v2), &v2));
     printf("%s & %s = %s\n", integerBits(sizeof(flags3), &flags1), integerBits(sizeof(mask), &mask), integerBits(sizeof(v3), &v3));
 }
+
+void turning_bits_on()
+{
+    uint8_t mask = 0b11010011;
+
+    uint8_t flags1 = 0b10101111;
+    uint8_t v1 = flags1 | mask;
+
+    uint8_t flags2 = 0b1111;
+    uint8_t v2 = flags2 | mask;
+
+    uint8_t flags3 = 0b11111000;
+    uint8_t v3 = flags3 | mask;
+
+    printf("%s | %s = %s\n", integerBits(sizeof(flags1), &flags1), integerBits(sizeof(mask), &mask), integerBits(sizeof(v1), &v1));
+    printf("%s | %s = %s\n", integerBits(sizeof(flags2), &flags1), integerBits(sizeof(mask), &mask), integerBits(sizeof(v2), &v2));
+    printf("%s | %s = %s\n", integerBits(sizeof(flags3), &flags1), integerBits(sizeof(mask), &mask), integerBits(sizeof(v3), &v3));
+}
+
--- golang/bitwise_op/usage_test.go
+++ golang/bitwise_op/usage_test.go
@@ -17,3 +17,19 @@ func Example_mask() {
 	// 1111 & 11010011 = 11
 	// 11111000 & 11010011 = 11010000
 }
+
+func Example_turning_bits_on() {
+	var mask uint8 = 0b11010011
+	var flags1 uint8 = 0b10101111
+	var flags2 uint8 = 0b1111
+	var flags3 uint8 = 0b11111000
+
+	fmt.Printf("%s | %s = %s\n", integerBits(flags1), integerBits(mask), integerBits(flags1|mask))
+	fmt.Printf("%s | %s = %s\n", integerBits(flags2), integerBits(mask), integerBits(flags2|mask))
+	fmt.Printf("%s | %s = %s\n", integerBits(flags3), integerBits(mask), integerBits(flags3|mask))
+
+	// Output:
+	// 10101111 | 11010011 = 11111111
+	// 1111 | 11010011 = 11011111
+	// 11111000 | 11010011 = 11111011
+}
