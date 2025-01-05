bc12c99fa33b8ed64affb34febd6fc5bade57973
dushaoshuai <1528606126@qq.com>
2025-01-05 18:20:01 +0800

    feat(slice): update

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+go mod tidy -x # go help mod
+
+go get -x go@1.23.4 # go help get
+# go mod edit -go=1.23.4 # go help mod edit
+
+go get -x toolchain@1.23.4 # go help get
+# go mod edit -toolchain=version # go help mod edit
+
+go get -u -x ./...
+go mod tidy -x
