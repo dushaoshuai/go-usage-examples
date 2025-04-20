9bb1d3ba634d506f2a26a03131caef276fdd3da2
shaouai <56815047+dushaoshuai@users.noreply.github.com>
2025-04-20 16:47:07 +0800

    feat(go mod): go tool directives
    * feat(go mod): go tool directives
    
    * feat(go mod): go get -x tool

---
+++ golang/x/tools/cmd/stringer/painkiller/painkiller.go
@@ -0,0 +1,26 @@
+package painkiller
+
+//go:generate go tool golang.org/x/tools/cmd/stringer -type=Pill,Weekday -output=gen_string.go -linecomment
+
+type Pill int
+
+const (
+	Placebo Pill = iota
+	Aspirin
+	Ibuprofen
+	Paracetamol
+	Acetaminophen = Paracetamol
+)
+
+// A Weekday specifies a day of the week (Sunday = 0, ...).
+type Weekday int
+
+const (
+	Sunday Weekday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
