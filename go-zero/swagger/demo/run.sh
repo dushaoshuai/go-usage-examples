199ad6112eac08551d17b6fc9a35805d0102197e
dushaoshuai <1528606126@qq.com>
2025-04-24 22:48:50 +0800

    no host

--- go-zero/swagger/demo/run.sh
+++ go-zero/swagger/demo/run.sh
@@ -5,4 +5,8 @@
 
 goctl api go --api ./swagger.api --dir . --style go_zero
 GOCTL_EXPERIMENTAL=on goctl api swagger --api ./swagger.api --dir ./internal/handler/swagger-ui-5.21.0/dist
+
+# https://swagger.io/docs/specification/v2_0/api-host-and-base-path/#host
+sed -i '/"host":/d' internal/handler/swagger-ui-5.21.0/dist/swagger.json
+
 go run .
