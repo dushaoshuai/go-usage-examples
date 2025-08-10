c69d78d9e322af7010934e20b10260257cdfce99
david_du <david.du@yuansuan.com>
2025-08-10 16:07:43 +0800

    fix(sync.Mutex): wait for goroutines to complete

--- golang/sync/Mutex_test.go
+++ golang/sync/Mutex_test.go
@@ -5,19 +5,39 @@ import (
 	"testing"
 )
 
+func TestMutex_normal_Unlock(t *testing.T) {
+	var mu sync.Mutex
+	defer func() {
+		if !mu.TryLock() {
+			t.Fatal("Mutex.TryLock() failed")
+		}
+		mu.Unlock()
+	}()
+
+	mu.Lock()
+	defer mu.Unlock()
+}
+
 // It is allowed for one goroutine to lock a Mutex and then
 // arrange for another goroutine to unlock it.
 func TestMutex_another_goroutine_unlock(t *testing.T) {
 	ch := make(chan struct{})
 	var mu sync.Mutex
+	var wg sync.WaitGroup
 
-	go func() {
+	wg.Go(func() {
 		mu.Lock()
 		ch <- struct{}{}
-	}()
+	})
 
-	go func() {
+	wg.Go(func() {
 		<-ch
 		mu.Unlock()
-	}()
+	})
+
+	wg.Wait()
+
+	// Ensure the mutex is indeed unlocked now.
+	mu.Lock()
+	mu.Unlock()
 }
