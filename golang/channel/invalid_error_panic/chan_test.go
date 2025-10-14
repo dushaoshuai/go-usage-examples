19416d8f8adaac5304ae6fed21e29f3a9a13c672
david_du <david.du@yuansuan.com>
2025-10-14 10:51:18 +0800

    feat(chan): comment invalid operation code

--- golang/channel/invalid_error_panic/chan_test.go
+++ golang/channel/invalid_error_panic/chan_test.go
@@ -2,25 +2,25 @@ package invalid_error_panic_test
 
 // It is an error if ch is a receive-only channel.
 func Example_close_a_receive_only_chan() {
-	var ch <-chan int = make(chan int)
 	// invalid operation: cannot close receive-only channel ch (variable of type <-chan int)
-	close(ch)
+	// var ch <-chan int = make(chan int)
+	// close(ch)
 
 	// Output:
 }
 
 func Example_send_to_a_receive_only_chan() {
-	var ch <-chan int = make(chan int)
 	// invalid operation: cannot send to receive-only channel ch (variable of type <-chan int)
-	ch <- 8
+	// var ch <-chan int = make(chan int)
+	// ch <- 8
 
 	// Output:
 }
 
 func Example_receive_from_a_send_only_chan() {
-	var ch chan<- int = make(chan int)
 	// invalid operation: cannot receive from send-only channel ch (variable of type chan<- int)
-	<-ch
+	// var ch chan<- int = make(chan int)
+	// <-ch
 
 	// Output:
 }
